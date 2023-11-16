14d7a2094d57b75dd7a7e1cb1472775f13604c58
khodmx <61280880+khodmx@users.noreply.github.com>
2023-11-16 19:58:21 +0300

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=a0872422_pilotazh',
+    'username' => 'a0872422_pilotazh',
+    'password' => 'ungiewikpu',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
